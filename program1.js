f1963d4cb787921fba4990f71be79dc6ac6c784a
Disha <dishatalreja1202@gmail.com>
2024-01-12 14:08:44 +0530

    Added program1 and program2

--- program1.js
+++ program1.js
@@ -2,9 +2,30 @@
  * @param {string} s
  * @return {boolean}
  */
+
+//(){}
 var isValid = function(s) {
-    
+    const stack = [];
+    const bracket = {
+        '(': ')',
+        '{': '}',
+        '[': ']'
+    };
+
+    for (let char of s) {
+        if (char in bracket) {
+            stack.push(char);
+        } else {
+            const top = stack.pop();
+            if (char !== bracket[top]) {
+                return false;
+            }
+        }
+    }
+
+    return stack.length === 0;
 };
+
 
 module.exports = { isValid };
 
--- program2.js
+++ program2.js
@@ -2,9 +2,43 @@
  * @param {string} s
  * @return {number}
  */
-var romanToInt = function(s) {
-    
+
+const roman = {
+    'I': 1,
+    'V': 5,
+    'X': 10,
+    'L': 50,
+    'C': 100,
+    'D': 500,
+    'M': 1000
 };
+
+var romanToInt = function(s) {
+    const romanNumerals = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    };
+
+    let result = 0;
+
+    for (let i = 0; i < s.length; i++) {
+
+        if (roman[s[i + 1]] > roman[s[i]]) {
+            result += roman[s[i + 1]] - roman[s[i]];
+            i++;
+        } else {
+            result += roman[s[i]];
+        }
+    }
+
+    return result;
+};
+
 
 
 module.exports={romanToInt}
